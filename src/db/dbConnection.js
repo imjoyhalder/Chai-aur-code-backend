c4425bb1a9a653f9ee3106080b1b18aa5e202c88
imjoyhalder <joyhalder00113355@gmail.com>
2025-09-21 00:32:46 +0600

    basic api create successfully

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose"
 import { DB_NAME } from "../constants.js"
 
-const connectDB = ()=>{
+const connectDB = async()=>{
     try {
-        const connectionInstance = mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n🚀MongoDB connection Host : ${connectionInstance}`)
-        return connectionInstance
+       
     } catch (error) {
         console.log('MONGODB Connection Error!!', error)
         process.exit(1)
